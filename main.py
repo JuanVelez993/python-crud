30d352d2515fc668e448bf7b778e7c5c1d4716b7
Juan Pablo Velez <juan.velez993@gmail.com>
2022-10-15 21:57:48 -0500

    basic crud completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
-from uuid import UUID, uuid4
-from fastapi import FastAPI
+from uuid import UUID
+from fastapi import FastAPI,HTTPException
 from models import models
 
 app=FastAPI()
@@ -29,3 +29,41 @@ async def root():
 @app.get('/api/v1/users')
 async def fetch_users():
     return db;
+#in the url you can user /doc and /redoc to see the documentation with swagger
+@app.post('/api/v1/users')
+async def create_user(user:models.User):
+    db.append(user)
+    return {'id': user.id}
+
+@app.delete('/api/v1/users/{user_id}')
+async def delete_user(user_id:UUID):
+    for user in db:
+        if user.id==user_id:
+            db.remove(user)
+            return
+    raise HTTPException(
+        status_code=404,
+        details=f'user with id {user_id} does not exist'
+    )
+
+@app.put('/api/v1/users/{user_id}')
+async def update_user(user_id:UUID,user_update:models.UserUpdate):
+    for user in db:
+        if user.id==user_id:
+            if user_update. first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update. last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update. middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update. roles is not None:
+                user. roles = user_update. roles
+            return
+    raise HTTPException(
+        status_code=404,
+        details=f'user with id {user_id} does not exist'
+    )
+
+
+    
+
--- models/models.py
+++ models/models.py
@@ -21,3 +21,11 @@ class User(BaseModel):
     middle_name:Optional[str]
     gender:Gender
     roles:List[Role]
+
+class UserUpdate(BaseModel):
+    id:Optional[UUID]=uuid4()
+    first_name:Optional[str]
+    last_name:Optional[str]
+    middle_name:Optional[str]
+    gender:Optional[Gender]
+    roles:List[Role]
